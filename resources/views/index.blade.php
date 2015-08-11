fb931e2bd193dbb4b069860d44d3d0b710a16fd7
Jayson R. Alanano <jayzer0098@gmail.com>
2015-08-12 00:04:53 +0800

    Fixed Home index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,7 @@
     </tr>
     <thead>
     <tr>
-      <th class="info">Vission</th>
+      <th class="info">Vision</th>
     </tr>
   </thead>
     <tr>
@@ -45,18 +45,4 @@
         	</div>
        	</div>
     </div>
-	{!! Form::open() !!}
-		<div class="form-group">
-			<div class="col-sm-8">
-				{!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username']) !!}
-				{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}<br>
-				{!! Form::submit('Login', ['class' => 'btn btn-primary']) !!}
-				<a href="" class="btn btn-primary">Register</a>
-			</div>
-
-		</div>
-	{!! Form::close() !!} -->
-
-
-
 @endsection
