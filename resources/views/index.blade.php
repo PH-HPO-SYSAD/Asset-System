da18d233af6a739dadc0b04293cba5e137b0d720
jayzer0098 <jayzer0098@gmail.com>
2015-08-12 08:27:24 +0800

    Dasboard v1.2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,41 +9,50 @@
 	</div>
 	<!-- Login Form -->
 	<div class="mission-vision">
-		<table class="table table-striped table-hover " align="left">
-  <thead>
-    <tr class="success">
-      <th>Mission</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>To achieve quality and love for work that can only be measured as excellent when dedicated for the Glory of God.</td>
-    </tr>
-    <thead>
-    <tr>
-      <th class="info">Vision</th>
-    </tr>
-  </thead>
-    <tr>
-      <td>To be a world class IT BPO Solutions Provider through people’s integrated and innovative ideas.</td>
-    </tr>
-  </tbody>
-</table>
+		<table class="table table-striped table-hover">
+      <thead>
+        <tr class="success">
+          <th>Mission</th>
+        </tr>
+      </thead>
+        <tbody>
+          <tr>
+            <td>To achieve quality and love for work that can only be measured as excellent when dedicated for the Glory of God.</td>
+          </tr>
+        <thead>
+          <tr>
+            <th class="info">Vision</th>
+          </tr>
+        </thead>
+        <tr>
+          <td>To be a world class IT BPO Solutions Provider through people’s integrated and innovative ideas.</td>
+        </tr>
+      </tbody>
+    </table>
 	</div>
-	<div class="login-container">
-        <div id="output"></div>
+  <div class="login-container" align="right">
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            
             <div class="jumbotron" align="center">
-            	<h3>Login</h3>
-            	<div class="form-group">
-                	{!! Form::open() !!}
-                    	{!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username']) !!}
-						{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}<br>
-						{{-- {!! Form::submit('Login', ['class' => 'btn btn-primary']) !!} temporary disabled --}}
-            <a href="{{ action('DashController@dashboard') }}" class="btn btn-primary">Login</a> 
-						<a href="{{ action('DashController@pincode') }}" class="btn btn-primary">Register</a>
-                	{!! Form::close() !!}
-            	</div>
-        	</div>
-       	</div>
-    </div>
+                <h4>Login</h4>
+                {!! Form::open() !!}
+                  <div class="form-group">
+                    {!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username']) !!}
+                    {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}<br>
+                    {{-- temporary disabled --}}
+                    <!-- {!! Form::submit('login', ['class' => 'btn btn-primary'])  !!} -->
+                    <a href="{{ action('DashController@dashboard') }}" class="btn btn-primary">Login</a>
+                    <a href="{{ action('DashController@register') }}" class="btn btn-primary">Register</a>
+                  </div>
+                {!! Form::close() !!}
+              </div>
+
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 @endsection
