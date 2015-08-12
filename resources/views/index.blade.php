6b9cb089a07b0be6774e30795702dd2f35fdf1e8
jayzer0098 <jayzer0098@gmail.com>
2015-08-12 13:57:23 +0800

    v1.5 Fixed Login CSS and Design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,58 +1,32 @@
 @extends('layout.master')
 @section('contents')
-	<div class="jumbo-header">
-		<div class="jumbotron" align="center">
-			<h2>Sysad Asset Inventory System</h2>
-			<p>Helping HPO Outsourcing Inc.</p>
-			<a href="#" class="btn btn-primary">Learn More</a>
-		</div>
-	</div>
-	<!-- Login Form -->
-	<div class="mission-vision">
-		<table class="table table-striped table-hover">
-      <thead>
-        <tr class="success">
-          <th>Mission</th>
-        </tr>
-      </thead>
-        <tbody>
-          <tr>
-            <td>To achieve quality and love for work that can only be measured as excellent when dedicated for the Glory of God.</td>
-          </tr>
-        <thead>
-          <tr>
-            <th class="info">Vision</th>
-          </tr>
-        </thead>
-        <tr>
-          <td>To be a world class IT BPO Solutions Provider through people’s integrated and innovative ideas.</td>
-        </tr>
-      </tbody>
-    </table>
-	</div>
-  <div class="login-container" align="right">
-    <table>
-      <tbody>
-        <tr>
-          <td>
-            
-            <div class="jumbotron" align="center">
-                <h4>Login</h4>
-                {!! Form::open() !!}
-                  <div class="form-group">
-                    {!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username']) !!}
-                    {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}<br>
-                    {{-- temporary disabled --}}
-                    <!-- {!! Form::submit('login', ['class' => 'btn btn-primary'])  !!} -->
-                    <a href="{{ action('DashController@dashboard') }}" class="btn btn-primary">Login</a>
-                    <a href="{{ action('DashController@pincode') }}" class="btn btn-primary">Register</a>
-                  </div>
-                {!! Form::close() !!}
-              </div>
+  <br><br>
 
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <h1 class="text-center login-title">Welcome to HPO Asset Inventory System</h1>
+            <div class="account-wall">
+                <img class="profile-img" src="https://c1.staticflickr.com/3/2361/5817626386_d3c0db3a02_b.jpg"
+                    alt="">
+                    {!! Form::open(['class' => 'form-signin']) !!}
+
+                        {!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username']) !!}
+                        {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+                        <a href="{{ action('DashController@dashboard') }}" class="btn btn-lg btn-primary btn-block">Login</a>
+                        <label class="checkbox pull-left">
+                    <input type="checkbox" value="remember-me">
+                      Remember me
+                </label>
+                <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                    {!! Form::close() !!}
+                {{-- <form class="form-signin">
+                <input type="text" class="form-control" placeholder="Email" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in</button> --}}
+                </form>
+            </div>
+            <a href="{{ action('DashController@pincode') }}" class="text-center new-account">Create an account </a>
+        </div>
+    </div>
 @endsection
